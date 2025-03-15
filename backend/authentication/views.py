b4624b523531cd0a5282c2539f1472a670f20102
PhatDongNai <phatnguyen5523@gmail.com>
2025-03-15 22:15:26 +0700

    validation register

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,7 +1,28 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
 
 # Create your views here.
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+    
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Sorry, this username is already taken. Please choose another one'}, status=409)
+        return JsonResponse({'username_valid': True})
+    
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'Sorry, this email is already taken. Please choose another one'}, status=409)
+        return JsonResponse({'email_valid': True})
