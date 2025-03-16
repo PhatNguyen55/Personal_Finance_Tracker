7a46f1aa7619e837fa5f237a0d8dfb53dd4badda
PhatDongNai <phatnguyen5523@gmail.com>
2025-03-16 23:19:52 +0700

    CRUD expences

--- backend/expenses/urls.py
+++ backend/expenses/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='expenses'),
-    path('add_expense', views.add_expense, name='add_expense'),
+    path('add-expense', views.add_expense, name="add-expenses"),
+    path('edit-expense/<int:id>', views.expense_edit, name="edit-expenses"),
+    path('expense_delete/<int:id>', views.delete_expense, name="expense-delete"),
 ]
