7bfaa4ffd7857ee8f113ec89bd9a007f3917c9cb
PhatDongNai <phatnguyen5523@gmail.com>
2025-03-17 16:01:02 +0700

    custom admin

--- backend/expenses/admin.py
+++ backend/expenses/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Expense, Category
 
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'owner', 'category', 'date',)
+    search_fields = ('description', 'category', 'date',)
+
+    list_per_page = 5
+
 # Register your models here.
 admin.site.register(Expense)
 admin.site.register(Category)
